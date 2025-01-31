78fa4f218b9f8789f26c10863bde1fb0172917ad
Fathum Mubin <fathummubin26@gmail.com>
2025-01-31 12:00:00 +0600

    Commit 1 for 2025-01-31

--- obj/Debug/net10.0/C#.AssemblyInfo.cs
+++ obj/Debug/net10.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bce821f540664f654107d58ccd2da39a70ad5d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c5e5e1342eaf0dbf9d196c3786c0cc1ecfa8463")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
