8e3123b95bc3872b5fcb14ce880707438310771b
Mubin <fathummubin26@gmail.com>
2026-01-19 01:10:53 +0600

    Update backend and frontend files

--- obj/Debug/net10.0/C#.AssemblyInfo.cs
+++ obj/Debug/net10.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38b130663aa5838fbbfc880f9f86a337041acdb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b33ed74864a5786aae2554e233b7fb5daea60de7")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
