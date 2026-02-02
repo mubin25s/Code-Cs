3aa16678de7aca4b73678eeb93520522c45cc1ce
Mubin <fathummubin26@gmail.com>
2026-02-03 02:23:50 +0600

    Update backend and frontend files

--- Simple Problems/ConditionalLogic.cs
+++ Simple Problems/ConditionalLogic.cs
@@ -17,7 +17,7 @@ namespace SimpleProblems
             }
             else if (score >= 70)
             {
-                Console.WriteLine("Grade: C");
+                Console.WrieLine("Grade: C");
             }
             else
             {
